a5cb8f3451d43241444e6d5f52a78133ef6ce32f
antodobrev <anton.dobrev@gmail.com>
2015-06-18 20:05:45 +0300

    fixing Default Namaspace

--- RPG-Game/Diablo/Interfaces/ICharacter.cs
+++ RPG-Game/Diablo/Interfaces/ICharacter.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Diablo.Interfaces
+namespace Diablo_0._5.Interfaces
 {
     public interface ICharacter
     {
-
+        int Health { get; }
+        int Damage { get; }
     }
 }
